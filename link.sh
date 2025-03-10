6015e16db9f3dab8b250152c228747f0fcabb10e
Lyx <madelen@duck.com>
2025-03-10 17:35:52 +0100

    Initial commit
    added my current dotfiles, and some scripts.

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Symlink creation script
+ln -sf ~/repos/dotfiles/sway/config ~/.config/sway/config
+ln -sf ~/repos/dotfiles/i3blocks/config ~/.config/i3blocks/config
+ln -sf ~/repos/dotfiles/bashrc/.bashrc ~/.bashrc
+ln -sf ~/repos/dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf ~/repos/dotfiles/nvim ~/.config/nvim
+ln -sf ~/repos/dotfiles/nvim-data ~/.config/nvim-data
