64f77390a75c47708ad594173c0f03bb2ac07d02
Lyx <madelen@duck.com>
2025-03-12 16:49:09 +0100

    added the link for wofi styling

--- link.sh
+++ link.sh
@@ -5,5 +5,4 @@ ln -sf ~/repos/dotfiles/sway/config ~/.config/sway/config
 ln -sf ~/repos/dotfiles/i3blocks/config ~/.config/i3blocks/config
 ln -sf ~/repos/dotfiles/bashrc/.bashrc ~/.bashrc
 ln -sf ~/repos/dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
-ln -sf ~/repos/dotfiles/nvim ~/.config/nvim
-ln -sf ~/repos/dotfiles/nvim-data ~/.config/nvim-data
+ln -sf ~/repos/dotfiles/wofi/style.css ~/.config/wofi/style.css
