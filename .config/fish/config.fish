9596ab78382455f9cbf7fe1504ad90503a1e97e5
Lyxminxx <130296296+Lyxminxx@users.noreply.github.com>
2025-06-27 00:08:29 +0200

    made ls run lsd -a

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-    alias ls='lsd'
     alias ll='lsd -alh'
     
     function fastfetch
     	echo ""
     	command fastfetch
     	echo ""
-    	ls
+    	lsd
     	echo ""
     end
     alias fetch='fastfetch'
+    alias ls='lsd -a'
 end
