2dcf782c71f97eb9d1b8e62ea738a227367b264e
lyxminxx <madelen@duck.com>
2026-03-29 18:56:39 +0200

    fixes to lsp

--- setup.sh
+++ setup.sh
@@ -12,6 +12,21 @@ echo Getting wallpaper
 mkdir ~/Pictures
 wget -O ~/Pictures/wall.png  https://gruvbox-wallpapers.pages.dev/thumbnails/anime/5m5kLI9.png 
 
+echo Installing lsps
+go install golang.org/x/tools/gopls@latest
+sudo npm install -g vscode-langservers-extracted \
+    typescript-language-server \
+    typescript \
+    bash-language-server \
+    prettier
+pipx install "python-lsp-server[all]"
+pipx install django-template-lsp
+
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
+
+fish_add_path ~/go/bin
+fish_add_path ~/.local/bin
+
 echo Stow
 stow cosmic
 stow nvim
