79371ce57a1e2814effbc2a14fae38ed047e08bf
lyxminxx <madelen@duck.com>
2026-04-02 17:44:50 +0200

    set helix as sudo editor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
-  set fish_greeting ""
-  alias ls 'lsd'
-  alias ll 'lsd -l'
-  alias la 'lsd -al'
-  set -U fish_user_paths $GOPATH/bin $HOME/.local/bin $fish_user_paths
+    set fish_greeting ""
+    alias ls lsd
+    alias ll 'lsd -l'
+    alias la 'lsd -al'
+    set -U fish_user_paths $GOPATH/bin $HOME/.local/bin $fish_user_paths
+    export SUDO_EDITOR=hx
 end
