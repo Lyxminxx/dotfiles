5d26884134bc0bcba7d9d04c08a2d875d07b7fef
Madelen Trygstad Lyngvær <Lyxminxx>
2026-06-30 21:21:56 +0200

    changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     alias ll 'lsd -l'
     alias la 'lsd -al'
     alias pac 'eval $(poetry env activate)'
+    alias hx helix
     alias notes 'hx ~/Documents/notes.md'
     alias cat bat
     set -U fish_user_paths $GOPATH/bin $HOME/.local/bin $fish_user_paths
